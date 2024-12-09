355f4dd4d49fa08de1d16efbf05d5a7e28b7a8a6
futile <git@futile.eu>
2024-12-09 02:38:28 +0000

    clippy suggestions

--- dump/src/main.rs
+++ dump/src/main.rs
@@ -4,14 +4,12 @@ use redb::{Database, ReadOnlyTable, ReadableTable, TableDefinition};
 
 const TABLE: TableDefinition<&str, u64> = TableDefinition::new("users");
 
-fn table_to_map(tb: ReadOnlyTable<&str, u64>) -> BTreeMap<String, u64> {
+fn table_to_map(tb: &ReadOnlyTable<&str, u64>) -> BTreeMap<String, u64> {
     let mut map: BTreeMap<String, u64> = BTreeMap::new();
     if let Ok(range) = tb.iter() {
-        for entry in range {
-            if let Ok(entry) = entry {
-                println!("{:?}: {}", entry.0.value().to_string(), entry.1.value());
-                map.insert(entry.0.value().to_string(), entry.1.value());
-            }
+        for entry in range.flatten() {
+            println!("{:?}: {}", entry.0.value().to_string(), entry.1.value());
+            map.insert(entry.0.value().to_string(), entry.1.value());
         }
     }
     map
@@ -21,7 +19,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db = Database::open("backups/users.bak.redb")?;
     let tx = db.begin_read()?;
     let tb = tx.open_table(TABLE)?;
-    let map = table_to_map(tb);
+    let map = table_to_map(&tb);
     let json = serde_json::to_string_pretty(&map)?;
     let mut file = File::create("backups/users.json")?;
     file.write_all(json.as_bytes())?;
