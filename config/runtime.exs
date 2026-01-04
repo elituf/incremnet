108d2cbb9c8706f12d76160402071e583d37263d
futile <git@futile.eu>
2026-01-04 03:03:28 +0000

    change default port to 34567

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,7 +2,7 @@ import Config
 
 config :incremnet,
   http_port:
-    System.get_env("INCREMNET_HTTP_PORT", "1337")
+    System.get_env("INCREMNET_HTTP_PORT", "34567")
     |> String.to_integer(),
   database_save_interval:
     :timer.seconds(60)
